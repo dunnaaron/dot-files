4f773cc23bc8aaa7772240ef834624501267f6ec
Aaron Dunn <dunnac1@gmail.com>
2025-07-16 21:45:24 -0400

    installed conform and set up formatters for lua and go

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,1 @@
 require("aaron")
-print("hello")
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,30 +1,87 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "Telescope",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("which-key").setup()
-    end,
-  },
-}
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
 
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<C-p>", function()
+				builtin.git_files()
+			end, { desc = "Telescope find git files" })
+			vim.keymap.set("n", "<leader>ps", function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end)
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" }, -- lazy-load on buffer read/new file
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"javascript",
+					"typescript",
+					"go",
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("which-key").setup()
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					go = { "gofmt" },
+					-- Conform will run the first available formatter
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+				},
+
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+			})
+
+			vim.keymap.set("n", "<leader>f", function()
+				require("conform").format({ async = true })
+			end, { desc = "Format file with Conform" })
+		end,
+	},
+}
